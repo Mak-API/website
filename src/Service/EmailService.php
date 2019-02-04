634a784fb6f771cfbe8f1a8ec6773a19f0d95a23
Jérémie Noël <dev.jnoel@gmail.com>
2019-02-04 12:32:28 +0100

    configure EmailService service

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -1,25 +1,50 @@
 <?php
 namespace App\Service;
+use Aws\Ses\SesClient;
+use Aws\Exception\AwsException;
+use Swift_Mailer;
+use Doctrine\ORM\EntityManagerInterface;
 
 class EmailService {
-    public function confirmRegistration($name, $email, $url, \Swift_Mailer $mailer)
+
+    /**
+     * @var \Swift_Mailer
+     *
+     */
+    private $mailer;
+    private $templating;
+
+    public function __construct(\Swift_Mailer $mailer, \Twig_Environment $templating, EntityManagerInterface $entityManager)
     {
+        $this->mailer = $mailer;
+        $this->templating = $templating;
+        $this->entityManager = $entityManager;
+    }
+
+    public function confirmRegistration($name, $email)
+    {
+        //$token = $this->gen_uuid();
+
+        //$user = $this->getDoctrine()
+        //    ->getRepository('UserBundle:User')
+        //    ->findOneByEmail($email);
+
+
+
             $message = (new \Swift_Message('Confirmez votre adresse email'))
-                ->setFrom('send@example.com')
+                ->setFrom(getenv('MAILER_FROM'))
                 ->setTo($email)
                 ->setBody(
-                    $this->renderView(
+                    $this->templating->render(
                     // templates/emails/registration.html.twig
                         'emails/registration.html.twig',
-                        ['name' => $name,
-                            'url' => $url]
+                        ['name' => $name]
+                        //['name' => $name,
+                        //    'token' => $token]
                     ),
                     'text/html'
                 );
-
-
-
-        $mailer->send($message);
+        $this->mailer->send($message);
     }
 
     public function resetPassword($email, $url,  \Swift_Mailer $mailer)
@@ -28,14 +53,36 @@ class EmailService {
             ->setFrom('send@example.com')
             ->setTo($email)
             ->setBody(
-                $this->renderView(
+                $this->templating->render(
                 // templates/emails/registration.html.twig
                     'emails/forgotPassword.html.twig',
                     ['url' => $url]
                 ),
                 'text/html'
             );
-        $mailer->send($message);
+        $this->mailer->send($message);
     }
+
+    //private function gen_uuid() {
+    //    return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+    //        // 32 bits for "time_low"
+    //        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+    //
+    //        // 16 bits for "time_mid"
+    //        mt_rand( 0, 0xffff ),
+    //
+    //        // 16 bits for "time_hi_and_version",
+    //        // four most significant bits holds version number 4
+    //        mt_rand( 0, 0x0fff ) | 0x4000,
+    //
+    //        // 16 bits, 8 bits for "clk_seq_hi_res",
+    //        // 8 bits for "clk_seq_low",
+    //        // two most significant bits holds zero and one for variant DCE1.1
+    //        mt_rand( 0, 0x3fff ) | 0x8000,
+    //
+    //        // 48 bits for "node"
+    //        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+    //    );
+    //}
 }
 
