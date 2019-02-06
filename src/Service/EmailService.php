217040751202c4459bdafaa29e0c146f59339bbd
Jérémie Noël <dev.jnoel@gmail.com>
2019-02-06 15:23:53 +0100

    update emailService (comment resetPwd, typo)

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -3,7 +3,6 @@ namespace App\Service;
 use Aws\Ses\SesClient;
 use Aws\Exception\AwsException;
 use Swift_Mailer;
-use Doctrine\ORM\EntityManagerInterface;
 
 class EmailService {
 
@@ -14,75 +13,63 @@ class EmailService {
     private $mailer;
     private $templating;
 
-    public function __construct(\Swift_Mailer $mailer, \Twig_Environment $templating, EntityManagerInterface $entityManager)
+    public function __construct(\Swift_Mailer $mailer, \Twig_Environment $templating)
     {
         $this->mailer = $mailer;
         $this->templating = $templating;
-        $this->entityManager = $entityManager;
     }
 
-    public function confirmRegistration($name, $email)
+    public function confirmRegistration($username, $email, $token)
     {
-        //$token = $this->gen_uuid();
-
-        //$user = $this->getDoctrine()
-        //    ->getRepository('UserBundle:User')
-        //    ->findOneByEmail($email);
-
-
-
-            $message = (new \Swift_Message('Confirmez votre adresse email'))
-                ->setFrom(getenv('MAILER_FROM'))
-                ->setTo($email)
-                ->setBody(
-                    $this->templating->render(
-                    // templates/emails/registration.html.twig
-                        'emails/registration.html.twig',
-                        ['name' => $name]
-                        //['name' => $name,
-                        //    'token' => $token]
-                    ),
-                    'text/html'
-                );
-        $this->mailer->send($message);
-    }
-
-    public function resetPassword($email, $url,  \Swift_Mailer $mailer)
-    {
-        $message = (new \Swift_Message('Mot de passe oublié ?'))
-            ->setFrom('send@example.com')
+        $message = (new \Swift_Message('Confirmez votre adresse email'))
+            ->setFrom(getenv('MAILER_FROM'))
             ->setTo($email)
             ->setBody(
                 $this->templating->render(
-                // templates/emails/registration.html.twig
-                    'emails/forgotPassword.html.twig',
-                    ['url' => $url]
+                    'emails/registration.html.twig',
+                    ['name' => $username,
+                        'token' => $token]
                 ),
                 'text/html'
             );
         $this->mailer->send($message);
     }
 
-    //private function gen_uuid() {
-    //    return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-    //        // 32 bits for "time_low"
-    //        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
-    //
-    //        // 16 bits for "time_mid"
-    //        mt_rand( 0, 0xffff ),
-    //
-    //        // 16 bits for "time_hi_and_version",
-    //        // four most significant bits holds version number 4
-    //        mt_rand( 0, 0x0fff ) | 0x4000,
-    //
-    //        // 16 bits, 8 bits for "clk_seq_hi_res",
-    //        // 8 bits for "clk_seq_low",
-    //        // two most significant bits holds zero and one for variant DCE1.1
-    //        mt_rand( 0, 0x3fff ) | 0x8000,
-    //
-    //        // 48 bits for "node"
-    //        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
-    //    );
+    //public function resetPassword($email, $url,  \Swift_Mailer $mailer)
+    //{
+    //    $message = (new \Swift_Message('Mot de passe oublié ?'))
+    //        ->setFrom(getenv('MAILER_FROM'))
+    //        ->setTo($email)
+    //        ->setBody(
+    //            $this->templating->render(
+    //                'emails/forgotPassword.html.twig',
+    //                ['url' => $url]
+    //            ),
+    //            'text/html'
+    //        );
+    //    $this->mailer->send($message);
     //}
+
+    function gen_uuid() {
+        return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            // 32 bits for "time_low"
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+            // 16 bits for "time_mid"
+            mt_rand( 0, 0xffff ),
+
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand( 0, 0x0fff ) | 0x4000,
+
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand( 0, 0x3fff ) | 0x8000,
+
+            // 48 bits for "node"
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+        );
+    }
 }
 
