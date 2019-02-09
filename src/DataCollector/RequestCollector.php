a6a6a150b405e7b70b705cd01c9fe33c7460bc03
Domange <backinsmail@gmail.com>
2019-02-09 19:39:54 +0100

    Add comment in RequestCollector

--- src/DataCollector/RequestCollector.php
+++ src/DataCollector/RequestCollector.php
@@ -9,6 +9,17 @@ use Symfony\Component\HttpKernel\DataCollector\DataCollector;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class RequestCollector
+ * @package App\DataCollector
+ *
+ * This function is used by Twig : /templates/data_collector/template.html.twig
+ * See the documentation here : https://symfony.com/doc/current/profiler/data_collector.html
+ *
+ * __construct and collect function add data in private $data array.
+ *
+ * How Twig get data ? `collector` variable calls the getters. So you need called getter and data's key by the same name.
+ */
 class RequestCollector extends DataCollector
 {
     public function __construct(UserRepository $userRepository)
