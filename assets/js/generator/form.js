09661ef0562724e882453219f6c780337f180240
Backins <backinsmail@gmail.com>
2019-02-15 00:08:37 +0100

    Edit name ok

--- assets/js/generator/form.js
+++ assets/js/generator/form.js
@@ -1,2 +1,23 @@
 import $ from 'jquery';
 
+let formApiName = document.getElementById('form-api-name');
+
+const onBlur = function(event) {
+    const value = this.value;
+    const text = document.createTextNode(value);
+    formApiName.removeChild(this);
+    formApiName.appendChild(text);
+};
+
+const onClick = function(event) {
+    const value = this.innerText;
+    let input = document.createElement('input');
+    input.className = 'test';
+    input.value = value;
+    this.innerText = '';
+    input.addEventListener('blur', onBlur);
+    this.appendChild(input);
+    input.focus();
+};
+
+formApiName.addEventListener('click', onClick);
