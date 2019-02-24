6498d14032d6691b47d07c281d7f990ca78fa938
Backins <backinsmail@gmail.com>
2019-02-24 20:22:17 +0100

    Add form. Wait merged of PR #79 to edit style

--- src/Controller/Back/CronTasksController.php
+++ src/Controller/Back/CronTasksController.php
@@ -3,10 +3,13 @@
 namespace App\Controller\Back;
 
 
+use App\Form\CronTasksType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\CronTasks;
 
 /**
  * Class CronTasksController
@@ -28,8 +31,16 @@ class CronTasksController extends AbstractController
     /**
      * @return Response
      */
-    public function add(): Response
+    public function add(Request $request): Response
     {
-        return $this->render('back/crontasks/add.html.twig');
+        $crontasks = new CronTasks();
+        $form = $this->createForm(CronTasksType::class, $crontasks);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump('test');
+        }
+        return $this->render('back/crontasks/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
--- src/Form/CronTasksType.php
+++ src/Form/CronTasksType.php
@@ -4,6 +4,12 @@ namespace App\Form;
 
 use App\Entity\CronTasks;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,17 +18,34 @@ class CronTasksType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('command')
-            ->add('options')
-            ->add('arguments')
-            ->add('expression')
-            ->add('lastExecution')
-            ->add('lastReturnCode')
-            ->add('logFile')
-            ->add('priority')
-            ->add('executeImmediately')
-            ->add('disabled')
+            ->add('name', TextType::class, [
+                'label' => 'Add description name of your task',
+            ])
+            ->add('command', TextType::class, [
+                'label' => 'Add command (example : "cache:clear")',
+            ])
+            ->add('options', TextType::class, [
+                'label' => 'Add option(s) (example : "-v")',
+                'required' => false,
+            ])
+            ->add('arguments', TextType::class, [
+                'label' => 'Add argument(s) (example : "argument1 argument2")',
+                'required' => false,
+            ])
+            ->add('expression', TextType::class, [
+                'label' => 'Add cron expression (example, for every 5 minutes : "*/5 * * * *")',
+            ])
+            ->add('logFile', TextType::class, [
+                'label' => 'Optional, add log file name without path',
+                'required' => false,
+            ])
+            ->add('priority', IntegerType::class, [
+                'data' => 1,
+                'required' => false,
+            ])
+            ->add('disabled', CheckboxType::class, [
+                'label' => 'Deactivate',
+            ])
         ;
     }
 
