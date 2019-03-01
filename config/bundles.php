63057a52c034a064d79b20ef34261e7d852e6b3b
Ivan Prunier <ivan.prunier@papernest.com>
2019-03-01 10:05:57 +0100

    Fix CORS

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
